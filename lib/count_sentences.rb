0229c5537228e39a283abe7406dcaf967d3d29f7
Prince <avecillasprince@yahoo.com>
2021-08-11 16:26:12 -0400

    Done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    y = self.split(/[.?!]/)
+    z = []
+    y.each do |x|
+      if x != ""
+        z << x
+      end
+    end
+    z.count
   end
 end
